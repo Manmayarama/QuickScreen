9107d9b0341e84f9b9234224710355025b32c288
Manmaya Rama K A <105375195+Manmayarama@users.noreply.github.com>
2025-08-25 22:26:32 +0530

    Update bookingController.js

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -49,7 +49,10 @@ export const createBooking = async (req, res) => {
     await showData.save();
 
     //Stripe gateway
-    const stripeInstance=new Stripe(process.env.STRIPE_SECRET_KEY);
+    const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2024-06-20",
+});
+
     //Creating line items for stripe
     const line_items=[{
       price_data: {
