05a9e56632459a857bf088e579266f75ef9c1ea5
Manmaya Rama <manmayarama@gmail.com>
2025-08-30 22:28:35 +0530

    Fix email recipient in sendBookingConfirmationEmail function to ensure correct user email is used

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -101,10 +101,11 @@ const sendBookingConfirmationEmail = inngest.createFunction(
                 path:'movie',
                 model: 'Movie'
             }
-        }).populate('user');
+        });
+        const user = await User.findById(booking.user); 
         
         await sendEmail({
-            to: booking.user.email,
+            to: user.email,
             subject: `Booking Confirmation - ${booking.show.movie.title}`,
             body: `
   <div style="font-family: Arial, sans-serif; background: #f8f9fa; padding: 20px; color: #333;">
