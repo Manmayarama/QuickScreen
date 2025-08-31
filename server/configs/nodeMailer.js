203f62ee225d64aff8f4821461fe49dadc2f424c
Manmaya Rama <manmayarama@gmail.com>
2025-08-31 20:07:52 +0530

    Refactor email transport configuration to use Gmail SMTP settings and update authentication details

--- server/configs/nodeMailer.js
+++ server/configs/nodeMailer.js
@@ -1,12 +1,13 @@
 import nodemailer from 'nodemailer'
 
 const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  auth: {
-    user: process.env.SMTP_USER, // Replace with your SMTP username
-    pass: process.env.SMTP_PASS, // Replace with your SMTP password
-  },
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'easyconsult11@gmail.com',
+        pass: 'whxicabidvowidub'
+    }
 });
 
 const sendEmail = async ({ to, subject, body }) => {
