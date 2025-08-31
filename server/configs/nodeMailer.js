b7bea699b4b240b2ffe4bfbc6b0a7c5875e36b6c
Manmaya Rama <manmayarama@gmail.com>
2025-08-31 19:06:45 +0530

    Refactor sendEmail function to improve parameter handling and add recipient validation

--- server/configs/nodeMailer.js
+++ server/configs/nodeMailer.js
@@ -9,14 +9,19 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-const sendEmail=async (to,subject,body) => {
-    const response = await transporter.sendMail({
-        from: process.env.SENDER_EMAIL,
-        to,
-        subject,
-        html: body,
-    });
-    return response;
+const sendEmail = async ({ to, subject, body }) => {
+  if (!to) {
+    throw new Error("❌ No recipient provided");
+  }
+
+  const response = await transporter.sendMail({
+    from: process.env.SENDER_EMAIL,
+    to,
+    subject,
+    html: body,
+  });
+  return response;
 };
+
 
 export default sendEmail;
